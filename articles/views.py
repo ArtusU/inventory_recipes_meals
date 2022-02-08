0dc4d537d7c7077f9545f7ee11b104f57281abdf
Artus U <artusrock@hotmail.com>
2022-02-08 12:10:02 +0000

    Article Search Manager Fixes #11

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,5 @@
 from operator import contains
 from re import A
-from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
@@ -11,11 +10,7 @@ from .models import Article
 
 def article_search_view(request):
     query = request.GET.get('q')
-    qs = Article.objects.all()
-    if query is not None:
-        lookups = Q(title__icontains=query) | Q(content__icontains=query)
-        qs = Article.objects.filter(lookups)
-        # qs = Article.objects.search(query)
+    qs = Article.objects.search(query=query)
     context = {
         "object_list": qs
     }

18abc0ad5b3647f880a88b634697e4dee29ff0eb
Artus U <artusrock@hotmail.com>
2022-02-11 12:38:41 +0000

    The Recipes App Fixes #17

--- articles/admin.py
+++ articles/admin.py
@@ -5,5 +5,7 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'user', 'timestamp', 'slug', 'updated']
     search_fields = ['title', 'content']
+    raw_id_fields = ['user']
+
 
 admin.site.register(Article, ArticleAdmin)

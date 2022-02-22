3f79675979fb50ab9a2e9f6302b72bd9e09daa47
Artus U <artusrock@hotmail.com>
2022-02-22 17:56:36 +0000

    Editing queryset - htmx Fixes #41

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.forms.models import modelformset_factory
-from django.http import HttpResponse
-from django.shortcuts import redirect, render, get_object_or_404
+from django.forms.models import modelformset_factory # model form for querysets
 from django.urls import reverse
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .forms import RecipeForm, RecipeIngredientForm
 from .models import Recipe, RecipeIngredient
-
+# CRUD -> Create Retrieve Update & Delete
 
 @login_required
 def recipe_list_view(request):
     qs = Recipe.objects.filter(user=request.user)
     context = {
-        'object_list': qs
+        "object_list": qs
     }
     return render(request, "recipes/list.html", context)
 
@@ -28,6 +28,8 @@ def recipe_detail_view(request, id=None):
 
 @login_required
 def recipe_detail_hx_view(request, id=None):
+    if not request.htmx:
+        raise Http404
     try:
         obj = Recipe.objects.get(id=id, user=request.user)
     except:
@@ -44,38 +46,63 @@ def recipe_detail_hx_view(request, id=None):
 def recipe_create_view(request):
     form = RecipeForm(request.POST or None)
     context = {
-        'form': form
-        }
+        "form": form
+    }
     if form.is_valid():
         obj = form.save(commit=False)
         obj.user = request.user
         obj.save()
         return redirect(obj.get_absolute_url())
-    return render(request, "recipes/create-update.html", context)
-
+    return render(request, "recipes/create-update.html", context)  
 
 @login_required
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
-    RecipeIngredientFormset = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0)
-    qs = obj.recipeingredient_set.all()
-    formset = RecipeIngredientFormset(request.POST or None, queryset=qs)
-    
+    new_ingredient_url = reverse("recipes:hx-ingredient-create", kwargs={"parent_id": obj.id})
     context = {
-        'form': form,
-        'formset': formset,
-        'object': obj
-    } 
-    if all([form.is_valid(), formset.is_valid()]):
-        recipe = form.save(commit=False)
-        recipe.save()
-        for ingredient_form in formset:
-            ingredient = ingredient_form.save(commit=False)
-            ingredient.recipe = recipe
-            ingredient.save()
-        context['message'] = 'Data saved'
+        "form": form,
+        "object": obj,
+        "new_ingredient_url": new_ingredient_url
+    }
+    if form.is_valid():
+        form.save()
+        context['message'] = 'Data saved.'
     if request.htmx:
         return render(request, "recipes/partials/forms.html", context)
-    return render(request, "recipes/create-update.html", context)
+    return render(request, "recipes/create-update.html", context)  
 
+
+@login_required
+def recipe_ingredient_update_hx_view(request, parent_id=None, id=None):
+    if not request.htmx:
+        raise Http404
+    try:
+        parent_obj = Recipe.objects.get(id=parent_id, user=request.user)
+    except:
+        parent_obj = None
+    if parent_obj is  None:
+        return HttpResponse("Not found.")
+    instance = None
+    if id is not None:
+        try:
+            instance = RecipeIngredient.objects.get(recipe=parent_obj, id=id)
+        except:
+            instance = None
+    form = RecipeIngredientForm(request.POST or None, instance=instance)
+    url = reverse("recipes:hx-ingredient-create", kwargs={"parent_id": parent_obj.id})
+    if instance:
+        url = instance.get_hx_edit_url()
+    context = {
+        "url": url,
+        "form": form,
+        "object": instance
+    }
+    if form.is_valid():
+        new_obj = form.save(commit=False)
+        if instance is None:
+            new_obj.recipe = parent_obj
+        new_obj.save()
+        context['object'] = new_obj
+        return render(request, "recipes/partials/ingredient-inline.html", context) 
+    return render(request, "recipes/partials/ingredient-form.html", context) 
